69b57c0b77255b636629a3c3018dcde74abbd547
crodpar637 <crodpar637@ieshnosmachado.org>
2025-11-25 09:46:51 +0100

    Auto save changes

--- hola_mundo/src/App.jsx
+++ hola_mundo/src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react';
-import Saludo from './Saludo.jsx';
-import Despedida from './Despedida.jsx';
-
+import { useState } from "react";
+import Saludo from "./Saludo.jsx";
+import Despedida from "./Despedida.jsx";
+import Boton from "./Boton.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const handleClickIncrementar = () => {
+  const handleClick = () => {
     setCount(count + 1);
   };
 
-
   return (
     <>
-      <h1>¡ Hola mundo !</h1>
+      <Boton count={count} manejador={handleClick} />
+      <Boton count={count} manejador={handleClick}/>
+      {/* <h1>¡ Hola mundo !</h1>
       <p>Este es el valor del contador: { count } </p>
       <button onClick={handleClickIncrementar}>Incrementar</button>
       <button onClick={ () => setCount(count -1)}>Decrementar</button>
@@ -22,9 +23,9 @@ function App() {
       <Saludo nombre="Carlos" apellido="Ramos"/>
       <Saludo nombre="Rosa" apellido="Ramos"/>
       <Saludo nombre="Pepe" />
-      <Despedida nombre="Juan" apellido="Ramos"/>
+      <Despedida nombre="Juan" apellido="Ramos"/> */}
     </>
-  )
+  );
 }
 
 export default App;
