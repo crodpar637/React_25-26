de4160609abcc3c56470b96993e02af47420d584
crodpar637 <crodpar637@ieshnosmachado.org>
2025-12-15 09:40:18 +0100

    Auto save changes

--- frontend_cine/src/App.jsx
+++ frontend_cine/src/App.jsx
@@ -1,11 +1,32 @@
+import { RouterProvider } from "react-router/dom";
+import { createBrowserRouter } from "react-router";
+import Inicio from "./components/Inicio";
 
+import Home from "./pages/Home";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Home,
+    children: [ // Todo esto se ve en el Outlet
+      {index:true, Component: Inicio }, // Esto se ve en la ruta padre
+      {
+        path: "/directors",
+        element: <h1>Listado de directores</h1>,
+      },
+      {
+        path: "/movies",
+        element: <h1>Listado de peliculas</h1>,
+      },
+    ],
+  },
+]);
 function App() {
   
 
   return (
     <>
-     
+      <RouterProvider router={router} />
     </>
   )
 }
